4450d85ee618378224780ab7e26fe70f1bd351c6
vkill <vkill.net@gmail.com>
2016-04-05 00:16:57 +0800

    change seeds

--- website/db/seeds.rb
+++ website/db/seeds.rb
@@ -4,3 +4,17 @@ Account.find_or_create_by!(email: Settings.super_admin.email) do |account|
   account.password = SecureRandom.hex
   account.skip_confirmation!
 end
+
+puts 'create bastion_hosts'
+case Settings.Arch_mode.to_i
+when 1
+  arch_mode = 1
+  ip = '127.0.0.1'
+  user = Settings.Arch_mode_one.user
+
+  bastion_host = BastionHost.find_or_initialize_by(arch_mode: arch_mode, ip: ip)
+  bastion_host.user = user
+  bastion_host.save!
+end
+
+
