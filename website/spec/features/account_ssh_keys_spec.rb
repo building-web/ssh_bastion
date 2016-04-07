9010a5db18d05866ef9a92b24f2a0202a90e2609
vkill <vkill.net@gmail.com>
2016-04-08 00:47:50 +0800

    change rspec

--- website/spec/features/account_ssh_keys_spec.rb
+++ website/spec/features/account_ssh_keys_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "AccountSshKeysIndex", type: :feature do
+RSpec.feature "AccountSshKeys", type: :feature do
 
   background do
     @user1 = create :user, password: 'password'
@@ -9,7 +9,9 @@ RSpec.feature "AccountSshKeysIndex", type: :feature do
     @user2 = create :user, password: 'password'
     @user2_ssh_public_key = generate_ssh_public_key('RSA', 'user2@example.com')
     cat, content, comment = @user2_ssh_public_key.split(' ')
-    create :account_ssh_key, account: @user2, title: 'user2_first_ssh_key', cat: cat, content: content, comment: comment
+    @user2_ssh_key = create :account_ssh_key, account: @user2, title: 'user2_first_ssh_key', cat: cat, content: content, comment: comment
+
+    @user2_new_ssh_public_key = generate_ssh_public_key('RSA', 'user2_new@example.com')
   end
 
   scenario 'user1 show ssh_key list' do
@@ -27,9 +29,11 @@ RSpec.feature "AccountSshKeysIndex", type: :feature do
 
     expect(page).to have_content 'user2_first_ssh_key'
     expect(page).to have_content ssh_public_key_fingerprint(@user2_ssh_public_key)
+
+    expect(page).to have_selector "a[href='/account/ssh_keys/#{@user2_ssh_key.id}']", text: :Delete
   end
 
-  scenario 'user1 add a ssh_key' do
+  scenario 'user1 add a new ssh_key' do
     sign_in_user_with @user1.email, 'password'
 
     visit '/account/ssh_keys'
@@ -61,7 +65,7 @@ RSpec.feature "AccountSshKeysIndex", type: :feature do
     expect(page).to have_content ssh_public_key_fingerprint(@user1_ssh_public_key)
   end
 
-  scenario 'user2 add a ssh_key' do
+  scenario 'user2 add a new ssh_key' do
     sign_in_user_with @user2.email, 'password'
 
     visit '/account/ssh_keys'
@@ -80,7 +84,68 @@ RSpec.feature "AccountSshKeysIndex", type: :feature do
     expect(page.find_field('Key').visible?).to eq true
     expect(page.find_button('Add SSH key').visible?).to eq true
 
+    within('form#new_ssh_key') do
+      fill_in 'Title', with: 'user2_new_ssh_key'
+      fill_in 'Key', with: @user2_new_ssh_public_key
+      click_button 'Add SSH key'
+    end
 
+    expect(page).to have_current_path('/account/ssh_keys')
+    user_sees_flash_notice 'SSH key was successfully created.'
+
+    expect(page).to have_content 'user2_new_ssh_key'
+    expect(page).to have_content ssh_public_key_fingerprint(@user2_new_ssh_public_key)
+  end
+
+  scenario 'user2 add a invalid ssh_key that Key is exists' do
+    sign_in_user_with @user2.email, 'password'
+
+    visit '/account/ssh_keys'
+
+    find('a#add_ssh_key').click
+
+    within('form#new_ssh_key') do
+      fill_in 'Title', with: 'user2_new_ssh_key'
+      fill_in 'Key', with: @user2_ssh_public_key
+      click_button 'Add SSH key'
+    end
+
+    expect(page).to have_current_path('/account/ssh_keys')
+    expect(page).to have_content 'Key is already in use'
+  end
+
+  scenario 'user2 add a invalid ssh_key that Key is invalid' do
+    sign_in_user_with @user2.email, 'password'
+
+    visit '/account/ssh_keys'
+
+    find('a#add_ssh_key').click
+
+    within('form#new_ssh_key') do
+      fill_in 'Title', with: 'user2_new_ssh_key'
+      fill_in 'Key', with: 'SSH_KEY'
+      click_button 'Add SSH key'
+    end
+
+    expect(page).to have_current_path('/account/ssh_keys')
+    expect(page).to have_content 'Key is invalid'
+  end
+
+  scenario 'user2 add a invalid ssh_key that Title is exists' do
+    sign_in_user_with @user2.email, 'password'
+
+    visit '/account/ssh_keys'
+
+    find('a#add_ssh_key').click
+
+    within('form#new_ssh_key') do
+      fill_in 'Title', with: 'user2_first_ssh_key'
+      fill_in 'Key', with: @user2_new_ssh_public_key
+      click_button 'Add SSH key'
+    end
+
+    expect(page).to have_current_path('/account/ssh_keys')
+    expect(page).to have_content 'Title is already in use'
   end
 
 end
