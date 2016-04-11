1d5279575f5e877b3592d708adc808a5a1c0192f
colorfulberry <senluowanxiangt@gmail.com>
2016-04-11 18:01:33 +0800

    feat: fxing the spec

--- website/app/controllers/account/account_ssh_keys_controller.rb
+++ website/app/controllers/account/account_ssh_keys_controller.rb
@@ -8,7 +8,7 @@ class Account::AccountSshKeysController < Account::BaseController
   def create
     @account_ssh_key = current_account.ssh_keys.new(ssh_key_params)
     if @account_ssh_key.save
-      redirect_to account_account_ssh_keys_path, notice: t('flash.actions.create.notice', resource_name: 'Ssh key')
+      redirect_to account_account_ssh_keys_path, notice: t('flash.actions.create.notice', resource_name: 'SSH key')
     else
 
       @account_ssh_keys = current_account.ssh_keys.page(params[:page])
@@ -20,7 +20,7 @@ class Account::AccountSshKeysController < Account::BaseController
   def destroy
     @account_ssh_key = current_account.ssh_keys.find(params[:id])
     if @account_ssh_key.destroy
-      redirect_to account_account_ssh_keys_path, notice: t('flash.actions.destroy.notice', resource_name: 'Ssh key')
+      redirect_to account_account_ssh_keys_path, notice: t('flash.actions.destroy.notice', resource_name: 'SSH key')
     else
       @account_ssh_keys = current_account.ssh_keys.page(params[:page])
 
