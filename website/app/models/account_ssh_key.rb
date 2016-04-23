07fbabf0d3403f8778e20b598a7101d75dba5e17
vkill <vkill.net@gmail.com>
2016-04-23 12:25:16 +0800

    remove default_value_for

--- website/app/models/account_ssh_key.rb
+++ website/app/models/account_ssh_key.rb
@@ -1,6 +1,6 @@
 class AccountSshKey < PublicKeyBox
 
-  default_value_for :public_key_boxable_type, 'Account'
+  attribute :public_key_boxable_type, :string, default: -> { 'Account' }
 
   belongs_to :account, class_name: 'Account', foreign_key: 'public_key_boxable_id'
 end
