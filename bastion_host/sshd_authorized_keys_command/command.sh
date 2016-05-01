8ea1d1adf03680fae894e47b114218973e9b6ceb
vkill <vkill.net@gmail.com>
2016-05-01 22:39:01 +0800

    add bastion_host scripts

---
+++ bastion_host/sshd_authorized_keys_command/command.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+config_file="$(pwd)/config"
+sub_command="$(pwd)/command_with_ssh_bastion.sh"
+
+typeset -A config
+
+config=(
+  [user]="developer"
+)
+
+while read line
+do
+  if echo $line | grep -F = &>/dev/null
+  then
+    varname=$(echo "$line" | cut -d '=' -f 1)
+    config[$varname]=$(echo "$line" | cut -d '=' -f 2-)
+  fi
+done < $config_file
+
+if [ "$1" == "${config[user]}" ]; then
+  . $sub_command
+fi
